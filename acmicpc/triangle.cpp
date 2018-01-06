d22e2bf303c8896c0fcee00e9d36502b5a720e30
sogogibeef <limjeongmin@wustl.edu>
2018-01-06 22:13:18 +0900

    Update triangle.cpp
    But why use vectors?

--- acmicpc/triangle.cpp
+++ acmicpc/triangle.cpp
@@ -2,8 +2,7 @@
 
 using namespace std;
 
-int triangleSumAllSides(vector<int> v), a, b, c, triangleMaxSide(vector<int> v);
-int d, e, f;
+int a, b, c;
 
 int main() {
 #ifndef ONLINE_JUDGE
@@ -13,12 +12,10 @@ int main() {
     ios_base::sync_with_stdio(0);
     //code here
     cin >> a >> b >> c;
-    vector<int> sides = {a, b, c};
-
     while ((a && b && c) != 0) {
-        if (triangleMaxSide(sides) >= (triangleSumAllSides(sides) - triangleMaxSide(sides))) {
+        int maximumSide = max(max(a, b), c);
+        if (maximumSide * 2 >= a + b + c) {
             cout << "Invalid" << '\n';
-
         } else {
             if ((a == b) && (a == c)) {
                 cout << "Equilateral" << '\n';
@@ -31,26 +28,8 @@ int main() {
             }
         }
         cin >> a >> b >> c;
-        sides = {a, b, c};
     }
-
     return 0;
 }
-
-
-int triangleSumAllSides(vector<int> v) {
-    int sum = 0;
-    for (int i: v) {
-        sum += i;
-    }
-    return sum;
-}
-
-
-int triangleMaxSide(vector<int> v) {
-    int maxSide = *max_element(v.begin(), v.end());
-    return maxSide;
-}
-
 
 
