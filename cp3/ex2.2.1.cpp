1332f1a8118c7c0bed67cc967b655480dc167619
sogogibeef <limjeongmin@wustl.edu>
2018-01-24 17:30:55 +0900

    Create ex2.2.1.cpp

---
+++ cp3/ex2.2.1.cpp
@@ -0,0 +1,32 @@
+//#include <bits/stdc++.h>
+#include "/Users/jeongminlim/stdc++.h"
+
+using namespace std;
+int T, e; // T = number of elements
+int MAX = 100 * 10^3; // 1 <= N <= 100k
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    //code here
+    cin >> T;
+    vector<int> numbers;
+    for (int i = 0; i < T; i++) {
+        cin >> e;
+        numbers.push_back(e);
+    }
+    bool pairs[100*1000] = {false,};
+
+    for (auto &i:numbers) {
+        if (pairs[i] == false) {
+            pairs[i] = true;
+        } else {
+            cout << i << " is duplicate\n";
+        }
+    }
+    return 0;
+
+}
+
