818fb0f01d2aa6b89359fd48a6ee9b2a49f59f36
sogogibeef <limjeongmin@wustl.edu>
2018-01-26 23:29:34 +0900

    Expert Enough?

---
+++ cp3/uva01237.cpp
@@ -0,0 +1,62 @@
+//#include <bits/stdc++.h>
+#include "/Users/jeongminlim/stdc++.h"
+
+using namespace std;
+int T; // T <=10
+int D; // Size of database
+string M; // name of maker
+int L, H; // Lowest / Highest price
+int Q; // number of query
+int P; // query price
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    //code here
+    cin >> T >> D;
+    cin.ignore();
+
+    for (int i = 0; i < T; i++) {
+        string makers[D];
+        int lowestPrice[D];
+        int highestPrice[D];
+
+        for (int j = 0; j < D; j++) {
+            string temp;
+            getline(cin, temp);
+            istringstream iss(temp);
+            iss >> M >> L >> H;
+            makers[j] = M;
+            lowestPrice[j] = L;
+            highestPrice[j] = H;
+        }
+
+
+        cin >> Q;
+        for (int j = 0; j < Q; j++) {
+            cin >> P;
+            int matches = 0;
+            int matchIndex = 0;
+            for (int k = 0; k < D; k++) {
+                if (lowestPrice[k] <= P && highestPrice[k] >= P) {
+                    matches++;
+                    matchIndex = k;
+                }
+            }
+            if (matches == 1) {
+                cout << makers[matchIndex] << '\n';
+            } else {
+                cout << "UNDETERMINED\n";
+            }
+
+        }
+
+        cin.ignore();
+
+    }
+
+    return 0;
+}
