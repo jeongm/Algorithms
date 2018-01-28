647e9e4a9fe1fd7f7ee97bf961bfc72de7722570
sogogibeef <limjeongmin@wustl.edu>
2018-01-28 11:39:14 +0900

    update Tour de France (Accepted)
    "Input consists of several test cases"

--- cp3/uva11242.cpp
+++ cp3/uva11242.cpp
@@ -12,43 +12,51 @@ int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
     //code here
-    cin >> f >> r;
-
     vector<int> frontTeeth;
     vector<int> rearTeeth;
     vector<double> driveRatios;
     vector<double> spread;
+    cin >> f;
+    while (f != 0) {
+        frontTeeth.clear();
+        rearTeeth.clear();
+        driveRatios.clear();
+        spread.clear();
 
-    for (int i = 0; i < f; i++) {
-        cin >> teeth;
-        frontTeeth.push_back(teeth);
-    }
+        cin >> r;
 
-    for (int i = 0; i < r; i++) {
-        cin >> teeth;
-        rearTeeth.push_back(teeth);
-    }
-
-    for (auto &i:frontTeeth) {
-        for (auto &j:rearTeeth) {
-            driveRatios.push_back((i / (double) j));
+        for (int i = 0; i < f; i++) {
+            cin >> teeth;
+            frontTeeth.push_back(teeth);
         }
-    }
 
-    sort(driveRatios.begin(), driveRatios.end());
-
-    for (int i = 0; i < driveRatios.size() - 1; i++) {
-        spread.push_back(driveRatios[i + 1] / driveRatios[i]);
-    }
-
-    double maxSpread = -1;
-    for (auto &i:spread) {
-        if (i > maxSpread) {
-            maxSpread = i;
+        for (int i = 0; i < r; i++) {
+            cin >> teeth;
+            rearTeeth.push_back(teeth);
         }
-    }
 
-    cout << setprecision(2) << fixed << maxSpread;
+        for (auto &i:frontTeeth) {
+            for (auto &j:rearTeeth) {
+                driveRatios.push_back((i / (double) j));
+            }
+        }
+        sort(driveRatios.begin(), driveRatios.end());
+
+        for (int i = 0; i < driveRatios.size() - 1; i++) {
+            spread.push_back(driveRatios[i + 1] / driveRatios[i]);
+        }
+
+        double maxSpread = -1;
+        for (auto &i:spread) {
+            if (i > maxSpread) {
+                maxSpread = i;
+            }
+        }
+
+        cout << setprecision(2) << fixed << maxSpread<<"\n";
+        cin >> f;
+
+    }
 
 
     return 0;
