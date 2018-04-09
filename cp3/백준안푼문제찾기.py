0279d74f81ecfd8bf23505c71e88f1c74d585b6f
Jeong Min Lim <limjeongmin@wustl.edu>
2018-04-10 01:45:29 +0900

    Update 백준안푼문제찾기.py

--- cp3/백준안푼문제찾기.py
+++ cp3/백준안푼문제찾기.py
@@ -1,14 +1,19 @@
 link = 'https://boj.kr/'
-raw = ''
-my = ''
+
+raw = 'some_recommendation'
 problems = list(raw.split(', '))
 
+my = 'some_solved'
 solved = list(my.split())
 
 v = open('problems_to_solve.txt', 'w')
-
+cnt = 0
+total = len(problems)
 for problem in problems:
     if problem not in solved:
+        cnt += 1
         v.write(link + problem + '\n')
 
 v.close()
+
+print(str(cnt) + ' problem links out of ' + str(total) + ' recommendations generated!')
