e8e0566d2acff3a6dfe6a97d67eecc3725c4eda9
Jeong Min Lim <limjeongmin@wustl.edu>
2018-04-10 00:26:59 +0900

    반자동 백준안푼문제찾기
    to be continued

---
+++ cp3/백준안푼문제찾기.py
@@ -0,0 +1,14 @@
+link = 'https://www.acmicpc.net/problem/'
+raw = ''
+my = ''
+problems = list(raw.split(', '))
+
+solved = list(my.split())
+
+v = open('problems_to_solve.txt', 'w')
+
+for problem in problems:
+    if problem not in solved:
+        v.write(link + problem + '\n')
+
+v.close()
